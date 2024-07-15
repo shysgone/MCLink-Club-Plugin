4bd5dac5d38de1bfac7c810c43b50ab34458550d
shy <temp2stuff@gmail.com>
2024-07-15 14:46:37 -0500

    commit ting

--- src/main/java/org/mclink/mclinkclub/commands/ClubCommand.java
+++ src/main/java/org/mclink/mclinkclub/commands/ClubCommand.java
@@ -32,11 +32,7 @@ public class ClubCommand implements CommandExecutor {
     // . . . . . . .
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String list, String[] args) {
-
-        if (!(sender instanceof Player)) {
-            Bukkit.getLogger().info("You can only execute commands in game");
-        }
-
+        if (!(sender instanceof Player)) { Bukkit.getLogger().info("You can only execute commands in game"); }
         assert sender instanceof Player;
         Player player = (Player) sender;
 
@@ -57,57 +53,13 @@ public class ClubCommand implements CommandExecutor {
                     }
 
                     if (args[0].equalsIgnoreCase("test")) {
-                        TextDisplay text = player.getWorld().spawn(player.getLocation(), TextDisplay.class);
-                        text.setText("text");
-                        //text.setBackgroundColor(Color.BLUE);
-                        text.setCustomName("test 2");
-                        text.setCustomNameVisible(true);
-                        text.setBillboard(Display.Billboard.CENTER);
-                        text.setGlowing(true);
-                        text.setGlowColorOverride(Color.RED);
-                        /*ArmorStand armorStand = (ArmorStand) player.getWorld().spawnEntity(player.getLocation().add(1.5, 0, 3), EntityType.ARMOR_STAND);
-                        armorStand.setGlowing(true);
-                        armorStand.setVisible(false);
-                        armorStand.setCustomNameVisible(true);
-                        armorStand.setCustomName(CC.translate("&d&lClub"));*/
-                        //CommandMenus.buildMenu(player, Arrays.asList("dad", "adad", "adad"), Arrays.asList("dad", "adad", "adad"), true);
+
                     }
                     break;
                 case 2:
                     if(args[0].equalsIgnoreCase("test") && args[1].equalsIgnoreCase("club")) {
                         try {
                             clubStand.addArmorStand(player);
-
-                            /*BlockDisplay clubBlock = player.getWorld().spawn(player.getLocation().add(1.5, 1, -0.5), BlockDisplay.class);
-                            clubBlock.setBlock(Bukkit.createBlockData(Material.GRASS_BLOCK));
-                            clubBlock.setBillboard(Display.Billboard.FIXED);
-
-                            BlockDisplay adminBlock = player.getWorld().spawn(player.getLocation().add(-0.5, 1, -0.5), BlockDisplay.class);
-                            adminBlock.setBlock(Bukkit.createBlockData(Material.COMMAND_BLOCK));
-                            adminBlock.setBillboard(Display.Billboard.FIXED);
-
-                            BlockDisplay playerBlock = player.getWorld().spawn(player.getLocation().add(-2.5, 1, -0.5), BlockDisplay.class);
-                            playerBlock.setBlock(Bukkit.createBlockData(Material.AMETHYST_BLOCK));
-                            playerBlock.setBillboard(Display.Billboard.FIXED);
-
-                            ArmorStand clubStand = (ArmorStand) player.getWorld().spawnEntity(player.getLocation().add(2, 0, 0), EntityType.ARMOR_STAND);
-                            clubStand.setGlowing(false);
-                            clubStand.setVisible(false);
-                            clubStand.setCustomNameVisible(true);
-                            clubStand.setCustomName(CC.translate("CLUB"));
-
-                            ArmorStand adminStand = (ArmorStand) player.getWorld().spawnEntity(player.getLocation().add(0, 0, 0), EntityType.ARMOR_STAND);
-                            adminStand.setGlowing(false);
-                            adminStand.setVisible(false);
-                            adminStand.setCustomNameVisible(true);
-                            adminStand.setCustomName(CC.translate("ADMIN"));
-
-                            ArmorStand playerStand = (ArmorStand) player.getWorld().spawnEntity(player.getLocation().add(-2, 0, 0), EntityType.ARMOR_STAND);
-                            playerStand.setGlowing(false);
-                            playerStand.setVisible(false);
-                            playerStand.setCustomNameVisible(true);
-                            playerStand.setCustomName(CC.translate("PLAYER"));*/
-
                         } catch (Throwable e) {
                             player.sendMessage("Error: " + e.getMessage());
                         }
